1785b13b3b36119157eee2e72cb8a63efee17aa1
Saurav kumar <kumarsaurav26198@gmail.com>
2022-01-23 23:39:48 +0530

    initital commmit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+let curDate = new Date(
+  // 2020, 12, 12, 19
+);
+curDate = curDate.getHours();
+let greeting = "";
+const cssStyle = {};
+if (curDate >= 1 && curDate < 12)
+{
+  greeting = "Good Morning";
+  cssStyle.color = "green";
+}
+else if (curDate >= 12 && curDate < 19)
+{
+  greeting = "Good AfterNoon";
+  cssStyle.color = "orange";
+}
+else if (curDate >= 19 && curDate < 24)
+{
+  greeting = "Good Night";
+  cssStyle.color = "Blue";
+}
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <>
+      <div className="boddyy">
+        {/* <h1>Welcome to Magnet World, it's {curDate} O'Clock.</h1> */}
+        <h2> Hello Sir, <span style={cssStyle}>{greeting}</span>.</h2>
+      </div>
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
